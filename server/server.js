0ebb67ca104c60784d22eff683ed72611a42925e
Miika <razoru@gmail.com>
2019-07-04 14:09:34 +0300

    Added bodyparser

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import P from 'bluebird'
 import dotenv from 'dotenv'
 import path from 'path'
+import bodyParser from 'body-parser'
 
 import logger from './logger'
 import bounties from './routes/index'
@@ -15,6 +16,9 @@ mongoose.Promise = P
 mongoose.connect(process.env.DBURI, { useNewUrlParser: true, promiseLibrary: P })
 
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use('/static', express.static(path.join(__dirname, 'static')))
