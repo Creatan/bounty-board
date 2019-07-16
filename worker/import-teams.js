9567b3820a81e48fe0ee120dc9ff02aeb8cc02ce
Miika <razoru@gmail.com>
2019-07-16 09:51:40 +0300

    Removed output from team import in support for scheduling it. Also reduced the amount of requests to rebbl.net

--- worker/import-teams.js
+++ worker/import-teams.js
@@ -3,8 +3,6 @@ import mongoose from 'mongoose'
 import P from 'bluebird'
 import request from 'request-promise'
 import dotenv from 'dotenv'
-import ora from 'ora'
-import chalk from 'chalk'
 
 import Team from '../server/models/team'
 import Season from '../server/models/season'
@@ -12,13 +10,15 @@ import Season from '../server/models/season'
 dotenv.config()
 mongoose.Promise = P
 mongoose.connect(process.env.DBURI, { useNewUrlParser: true, promiseLibrary: P })
+const args = process.argv.slice(2)
 
-const season = process.argv[2]
+const season = args[0]
 
 if (!season) {
   console.log('Usage: Provide season name')
   process.exit(1)
 }
+
 const allowedLeagues = ['REBBL - REL', 'REBBL - GMan', 'REBBL - Big O']
 
 const delay = ms => new P(resolve => setTimeout(resolve, ms));
@@ -34,43 +34,39 @@ const delay = ms => new P(resolve => setTimeout(resolve, ms));
     if (allowedLeagues.every(val => leagues.includes(val))) {
       await P.map(allowedLeagues, async (league) => {
         const teams = await request.get(`https://rebbl.net/api/v2/standings/${league}/${season}`, { json: true })
-        let count = 1
-        const spinner = ora(`Importing teams for ${league}: ${count}/${teams.length}`).start()
         await P.map(teams, async (team) => {
           const oldTeam = await Team.findOne({ id: team.teamId }).exec()
-          const url = `https://rebbl.net/api/v2/team/${team.teamId}/players`
-          const playerData = await request(url, { json: true })
 
-          const roster = playerData.map(player => ({
-            id: player.id,
-            name: player.name,
-          }))
-          if (!oldTeam) {
-            const newTeam = new Team({
-              id: team.teamId,
-              name: team.team,
-              league,
-              division: team.competition,
-              seasons: [season],
-              roster,
-            })
+          if (oldTeam && !oldTeam.seasons.includes(season)) {
+            const url = `https://rebbl.net/api/v2/team/${team.teamId}/players`
+            const playerData = await request(url, { json: true })
 
-            await newTeam.save()
-          } else {
-            oldTeam.roster = roster
-            oldTeam.division = team.competition
-            if (!oldTeam.seasons.includes(season)) {
+            const roster = playerData.map(player => ({
+              id: player.id,
+              name: player.name,
+            }))
+            if (!oldTeam) {
+              const newTeam = new Team({
+                id: team.teamId,
+                name: team.team,
+                league,
+                division: team.competition,
+                seasons: [season],
+                roster,
+              })
+
+              await newTeam.save()
+            } else {
               oldTeam.seasons.push(season)
+
+              oldTeam.roster = roster
+              oldTeam.division = team.competition
+              await oldTeam.save()
             }
 
-            await oldTeam.save()
+            await delay(100)
           }
-          count += 1
-
-          spinner.text = `Importing teams for ${league}: ${count}/${teams.length}`
-          await delay(100)
         }, { concurrency: 1 })
-        spinner.stopAndPersist({ symbol: `${chalk.green.bold('✓')}`, text: `Importing teams for ${league}: Done` })
       }, { concurrency: 1 })
     }
   } catch (err) {
