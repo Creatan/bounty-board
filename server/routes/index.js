035e208092fb7d321fd2fc40d26c524f2abb8c03
Miika <razoru@gmail.com>
2019-08-12 14:19:40 +0300

    Changed routes to return json and added validation for creation

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,33 +1,30 @@
 import express from 'express'
 import Bounty from '../models/bounty'
-import Season from '../models/season'
 import Team from '../models/team'
 import { authenticated, HttpError } from '../utils'
 
 const router = express.Router()
 
-function transformSeasonData(season) {
-  const leagues = season.leagues.map(league => ({
-    value: league.name,
-    label: league.name.split(' - ')[1],
-  }))
-  const divisions = season.leagues.reduce((acc, cur) => acc.concat(cur.divisions
-    .map(div => ({ value: `${cur.name}:${div}`, label: div.split(' - ')[1] }))), [])
-    .sort((a, b) => {
-      if (a.label > b.label) return 1
-      if (a.label < b.label) return -1
-      return 0
-    })
-  return { leagues, divisions }
+function capitalize(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1)
+}
+function isValid(bounty) {
+  const validationErrors = []
+  Object.entries(bounty).forEach(([key, value]) => {
+    if (!value) {
+      validationErrors.push(`${capitalize(key)} is required`)
+    }
+  })
+
+  return validationErrors
 }
 
 async function listBounties(req, res) {
   const bounties = await Bounty.find().sort({ league: 1, 'team.name': 1 }).exec()
-  const season = await Season.findOne({ active: true }).exec()
-  res.render('index', { bounties, season: { ...transformSeasonData(season), identifier: season.identifier }, user: req.user })
+  res.json(bounties)
 }
 
-async function createBounty(req, res) {
+async function createBounty(req, res, next) {
   const {
     league,
     division,
@@ -39,32 +36,37 @@ async function createBounty(req, res) {
   } = req.body
 
   // TODO: Request validation
-
-  const teamData = await Team.findOne({ id: team }).exec()
-  const playerData = teamData.roster.find(p => p.id === parseInt(player, 10))
-  const bounty = new Bounty({
-    league,
-    division: division.split(':')[1],
-    team: {
-      id: teamData.id,
-      name: teamData.name,
-    },
-    player: {
-      id: playerData.id,
-      name: playerData.name,
-    },
-    requirement,
-    reason,
-    prize,
-    status: 'open',
-    provider: {
-      id: req.user._id,
-      name: req.user.redditName,
-    },
-    season: 'season 11',
+  const validationErrors = isValid({
+    league, division, team, player, requirement, reason, prize,
   })
-  await bounty.save()
-  res.redirect('/')
+  if (validationErrors.length > 0) {
+    next(new HttpError(400, validationErrors.join(',')))
+  } else {
+    const teamData = await Team.findOne({ id: team }).exec()
+    const playerData = teamData.roster.find(p => p.id === parseInt(player, 10))
+    const bounty = new Bounty({
+      league,
+      division: division.split(':')[1],
+      team: {
+        id: teamData.id,
+        name: teamData.name,
+      },
+      player: {
+        id: playerData.id,
+        name: playerData.name,
+      },
+      requirement,
+      reason,
+      prize,
+      status: 'open',
+      provider: {
+        id: req.user._id,
+        name: req.user.redditName,
+      },
+      season: 'season 11',
+    })
+    res.json(await bounty.save())
+  }
 }
 
 async function deleteBounty(req, res, next) {
@@ -80,7 +82,7 @@ async function deleteBounty(req, res, next) {
 
 
 router.get('/', listBounties)
-router.post('/bounty', authenticated, createBounty)
-router.delete('/bounty/:id', authenticated, deleteBounty)
+router.post('/', authenticated, createBounty)
+router.delete('/:id', authenticated, deleteBounty)
 
 export default router
