a26777d1f29c8a9c3124c629f8d02ad3a446c78e
Miika <razoru@gmail.com>
2019-08-12 14:08:32 +0300

    rename eslintrc.js and have separate rules for client

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "extends": "airbnb-base",
-    "rules": {
-        "semi": ['error', 'never'],
-        "max-len": ['error', { code: 150 }],
-        "no-underscore-dangle": ['error', {allow: ['_id', '__v']}],
-    }
-};
