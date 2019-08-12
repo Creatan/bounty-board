cfe9b45ce4ba2f3ec608a4b7673d1ec884d9c474
Miika <razoru@gmail.com>
2019-08-12 14:20:01 +0300

    Build query for teams

--- server/routes/team.js
+++ server/routes/team.js
@@ -6,8 +6,11 @@ const router = express.Router()
 
 async function getTeams(req, res) {
   const { division, league, season } = req.query
-
-  const teams = await Team.find({ division, league, seasons: { $in: [season] } }).exec()
+  const query = {}
+  if (division) query.division = division
+  if (league) query.league = league
+  if (season) query.seasons = { $in: [season] }
+  const teams = await Team.find(query).exec()
   res.json(teams)
 }
 
