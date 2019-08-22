b5aaa8af8f46dd8de4636e7d127df833754273d9
Miika <razoru@gmail.com>
2019-08-22 12:14:36 +0300

    script for adding a bunch of bounties for testing

---
+++ utilities/create-bounties-batch.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+import mongoose from 'mongoose'
+import P from 'bluebird'
+import dotenv from 'dotenv'
+
+import Bounty from '../server/models/bounty'
+
+dotenv.config()
+mongoose.Promise = P
+mongoose.connect(process.env.DBURI, { useNewUrlParser: true, promiseLibrary: P });
+(async () => {
+  const bountyBase = await Bounty.findOne({}).exec()
+
+  const { _id, __v, ...bountyData } = bountyBase.toObject()
+  let counter = 0
+  const promises = []
+  while (counter < 50) {
+    promises.push(new Bounty(bountyData).save())
+    counter += 1
+  }
+  await P.all(promises)
+  mongoose.connection.close()
+})()
