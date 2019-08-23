bb54a6fa8032e52c6a789314424219eabbe31346
Miika <razoru@gmail.com>
2019-08-23 12:30:57 +0300

    Prepopulate create modal with values if player querystring is set

--- server/routes/team.js
+++ server/routes/team.js
@@ -5,12 +5,17 @@ const router = express.Router()
 
 
 async function getTeams(req, res) {
-  const { division, league, season } = req.query
+  const {
+    division, league, season, player,
+  } = req.query
   const query = {}
   if (division) query.division = division
   if (league) query.league = league
-  if (season) query.seasons = { $in: [season] }
+  if (season) query.seasons = season
+  if (player) query['roster.id'] = +player
+  console.log(query)
   const teams = await Team.find(query).exec()
+  console.log(teams)
   res.json(teams)
 }
 
